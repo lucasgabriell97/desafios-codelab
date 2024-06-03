66b4f14164e5ec36c1fc815dbe537573b00e8a90
Lucas Gabriell <lucas.gabriell97@hotmail.com>
2024-06-03 01:36:29 -0300

    feat: finalizando desafio-01 com animações css e javascript

---
+++ desafio-01/js/script.js
@@ -0,0 +1,14 @@
+// Fill heart icon on click
+const iconHeart = document.querySelectorAll(".icon-heart");
+
+iconHeart.forEach(icon => {
+  icon.onclick = () => {
+    icon.classList.toggle("filled");
+  };
+});
+
+// Scroll Reveal
+ScrollReveal().reveal('.card', { delay: 200, reset: true });
+ScrollReveal().reveal('.date', { delay: 300, reset: true });
+ScrollReveal().reveal('h2', { delay: 400, reset: true });
+ScrollReveal().reveal('p', { delay: 500, reset: true });
