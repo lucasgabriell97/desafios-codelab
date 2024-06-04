6919b24d5027df857b0b77b3f8c37907441ff54a
Lucas Gabriell <lucas.gabriell97@hotmail.com>
2024-06-04 03:14:21 -0300

    style: correções no uso correto dos espaçamentos

--- desafio-01/js/script.js
+++ desafio-01/js/script.js
@@ -9,7 +9,6 @@ iconHeart.forEach(icon => {
 
 // Scroll Reveal
 ScrollReveal().reveal('.card', { delay: 200, reset: true });
-ScrollReveal().reveal('.date', { delay: 300, reset: true });
-ScrollReveal().reveal('h2', { delay: 400, reset: true });
-ScrollReveal().reveal('p', { delay: 500, reset: true });
-ScrollReveal().reveal('footer', { delay: 600, reset: true });
+ScrollReveal().reveal('.card .date', { delay: 300, reset: true });
+ScrollReveal().reveal('.card h2', { delay: 400, reset: true });
+ScrollReveal().reveal('.card p', { delay: 500, reset: true });
