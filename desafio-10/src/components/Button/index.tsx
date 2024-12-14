6620d1254f15f1a66a0d49b4a04cb680e865fa66
Lucas Gabriell <lucas.gabriell97@hotmail.com>
2024-12-14 01:45:23 -0300

    feat: adicionando função para exibir os dias restantes para o natal de forma dinâmica

--- desafio-10/src/components/Button/index.tsx
+++ desafio-10/src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button: React.FC = () => {
   return (
-    <button className="bg-brand-color-red text-white font-bold uppercase rounded py-4 w-full md:w-60">
+    <button className="bg-brand-color-red hover:bg-opacity-80 text-white font-bold uppercase rounded py-4 w-full md:w-60">
       Fazer uma doação
     </button>
   );
--- desafio-10/src/layouts/TimerSection/index.tsx
+++ desafio-10/src/layouts/TimerSection/index.tsx
@@ -3,6 +3,40 @@ import React from "react";
 import IllustrativeChristmasImage from "../../assets/images/illustration-1.svg";
 
 export const TimerSection: React.FC = () => {
+  const [timeRemaining, setTimeRemaining] = React.useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  React.useEffect(() => {
+    const calculateTimeRemaining = () => {
+      const now = new Date();
+      const currentYear = now.getFullYear();
+      const christmasDate = new Date(currentYear, 11, 25);
+
+      if (now > christmasDate) {
+        christmasDate.setFullYear(currentYear + 1);
+      }
+
+      const difference = christmasDate.getTime() - now.getTime();
+
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((difference / (1000 * 60)) % 60);
+      const seconds = Math.floor((difference / 1000) % 60);
+
+      setTimeRemaining({ days, hours, minutes, seconds });
+    };
+
+    const timer = setInterval(calculateTimeRemaining, 1000);
+
+    calculateTimeRemaining();
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <section className="border-b-2 border-b-brand-color-pink py-20 px-6">
       <div className="flex flex-col items-center">
@@ -12,7 +46,7 @@ export const TimerSection: React.FC = () => {
           compartilhe a alegria do Natal.
         </p>
         <h3 className="text-3xl md:text-6xl font-semibold text-center text-brand-color-red mb-6 md:mb-10">
-          10d - 24h - 01m - 52s
+          {`${timeRemaining.days}d - ${timeRemaining.hours}h - ${timeRemaining.minutes}m - ${timeRemaining.seconds}s`}
         </h3>
         <div className="flex justify-center">
           <img
